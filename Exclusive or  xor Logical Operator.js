c4be776983ea1bba8907abddd18fcc67df5c301f
Alina <alina.navitskaya.uk@gmail.com>
2023-02-28 22:35:50 +0000

    hello

---
+++ Exclusive or  xor Logical Operator.js
@@ -0,0 +1,12 @@
+function xor(a, b) {
+    if (a && !b){
+      return true ; 
+    }
+    else if(a && b){
+       return false;
+    }
+    else if (!a && b){
+      return true;
+    }
+    else return false;
+  }
