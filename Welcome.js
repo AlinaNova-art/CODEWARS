34c4651dea03b06dabe31cf64d1bb30938956841
Alina <alina.navitskaya.uk@gmail.com>
2023-03-16 21:04:29 +0000

    hello

---
+++ Welcome.js
@@ -0,0 +1,29 @@
+var database = new Object();
+
+var database = {
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+};
+
+
+function greet(language) {
+	if (database.hasOwnProperty(language)){
+    return database[language];
+  }
+return "Welcome";
+  }
